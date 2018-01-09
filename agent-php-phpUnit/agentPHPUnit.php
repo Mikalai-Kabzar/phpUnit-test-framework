653d65f86f4e2abc57bbddc4bf683696b1526f53
Mikalai Kabzar <Mikalai_Kabzar@epam.com>
2018-01-09 16:29:52 +0300

    add listener class

---
+++ agent-php-phpUnit/agentPHPUnit.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mikalai_Kabzar
+ * Date: 1/9/2018
+ * Time: 1:47 PM
+ */
+use PHPUnit_Framework_TestCase as TestCase;
+use PHPUnit_Framework_TestListener as TestListener;
+
+class agentPHPUnit extends TestListener
+{
+
+}
